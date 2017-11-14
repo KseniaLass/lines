7c73c8d1ce4a0472ab90d3621534af0138a97152
KDekho01 <ksenia.dekhonova@isobar.ru>
2017-11-14 18:33:17 +0300

    append first items to canvas

--- lines.js
+++ lines.js
@@ -1,20 +1,22 @@
 (function(){
-  var canvas = null;
-  var ctx = null;
-  var spritesheet = null;
-  var spritesheetLoaded = false;
-  var world = [];
-  var worldWidth = 9;
-  var worldHeight = 9;
+  let canvas = null;
+  let ctx = null;
+  let spritesheet = null;
+  let spritesheetLoaded = false;
+  let world = [];
+  let worldWidth = 9;
+  let worldHeight = 9;
 
-  var tileWidth = 50;
-  var tileHeight = 50;
+  let figureCount = 9;
 
-  var pathStart = [worldWidth, worldHeight];
-  var pathEnd = [0,0];
-  var currentPath = [];
+  let tileWidth = 50;
+  let tileHeight = 50;
 
-  $('document').ready(function(e){
+  let pathStart = [worldWidth, worldHeight];
+  let pathEnd = [0,0];
+  let currentPath = [];
+
+  $(document).ready(function(){
     console.log('Cтраница загружена.');
     canvas = $('#gameCanvas')[0];
     canvas.width = worldWidth * tileWidth;
@@ -28,5 +30,63 @@
 
   function canvasClick(){}
 
-  function loaded(){}
+  function loaded(){
+    console.log("Картинка загружена");
+    spritesheetLoaded = true;
+    createWorld();
+  }
+
+  function createWorld(){
+    console.log('Создание мира...');
+    for(let x = 0; x < worldWidth; x++) {
+      world[x] = [];
+      for(let y=0; y < worldHeight; y++) {
+        world[x][y] = 0;
+      }
+    }
+    createRandomBalls();
+  }
+
+  function createRandomBalls() {
+    console.log('Расставляем шарики...');
+    for(let i=0; i<3; i++) {
+      let randomX = Math.floor(Math.random() * (worldWidth - 0)) + 0;
+      let randomY = Math.floor(Math.random() * (worldHeight - 0)) + 0;
+      let randomFigure = Math.floor(Math.random() * (figureCount+1 - 1)) + 1;
+      if(world[randomX][randomY] === 0) {
+        world[randomX][randomY] = randomFigure
+      } else {
+        i--;
+        break;
+      }
+    }
+    redraw();
+  }
+
+  function redraw() {
+
+    if(!spritesheetLoaded) return;
+
+    console.log('Рисуем мир ...');
+
+
+    let spriteNum = 0;
+
+    ctx.fillStyle='#000000';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+
+    for(let x = 0; x < worldWidth; x++) {
+      for(let y = 0; y < worldHeight; y++) {
+        spriteNum = world[x][y];
+        ctx.drawImage(spritesheet,
+          spriteNum*tileWidth, 0,
+          tileWidth, tileHeight,
+          x*tileWidth, y*tileHeight,
+          tileWidth, tileHeight);
+      }
+    }
+  }
+
+
+
 })();
